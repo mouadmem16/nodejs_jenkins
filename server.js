bcd3b35e3704b5cddf80a4f3bef61932f2afb443
ELMASLOUHY Mouaad <mouad.mem16@gmail.com>
2021-01-31 20:43:30 +0100

    new changes

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello From Node js Server, Mouaad ELMASLOUHY </h1>');
 });
 
+app.get('/admin', (req, res) => {
+  res.send('<h1>Hello From Admin </h1>');
+});
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
